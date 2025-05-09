41d8ed3148ff20eb5c91c374cbf64cd262898340
Andrew Matsumura <42308941+glader0293@users.noreply.github.com>
2025-05-09 20:36:17 +0000

    Final version

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,24 +1,63 @@
 import React from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faX } from '@fortawesome/free-solid-svg-icons'
+import "@fontsource/work-sans/300.css"; 
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
 // css
 import "../../styles/index.css"
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState ([]);
 	return (
-		<div className="container bg-white d-flex align-items-center justify-content-center flex-column">
+		<div className="container bg-light d-flex align-items-center justify-content-center flex-column w-50 h-40">
 			<h1>todos</h1>
-			<div className="container-box">
-				<ul>
-					<li><input type="text" placeholder="What do you need to do"></input></li>
-					<li>Take a shower</li>
-					<li>Sleep a lot</li>
-					<li>Place a market order</li>
-					<li>Do some homework</li>
+			
+			<div className="container-box d-flex justify-content-between flex-column bg-white shadow bg-white">
+				<ul className="p-0">
+					
+						<input type="text"
+								className="border-bottom px-3 py-2"
+								onChange={(e) => setInputValue(e.target.value)}
+								value= {inputValue}
+								onKeyPress = {(e)=> {
+									if(e.key === "Enter"){
+										setTodos(todos.concat(inputValue));
+									}
+								}} 
+								placeholder="What do you need to do">
+						</input>
+					
+					
+					{todos.map((item,index) => (
+							<div className="d-flex align-items-center justify-content-between icon hover-item p-0">
+								<li className="px-3 py-2">{item}</li> 
+								<FontAwesomeIcon className="icon-hidden icon-color px-3 py-2" 
+								onClick={() =>
+								setTodos(
+									todos.filter(
+										(t, currentIndex) =>
+											index != currentIndex
+									)
+								)	
+								}
+								icon={faX} size="2xs" />
+								
+								
+							</div>
+					))}
+					
+					
+					
+				
 				</ul>
+				<span className="d-flex align-items justify-content-start px-3 py-2 m-0">{todos.length} items left</span>
 			</div>
+			
 		</div>
 	);
 };
